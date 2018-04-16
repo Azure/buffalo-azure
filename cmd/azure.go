a5b0ab60cfe6ccaf5c862a752e484d95f7004284
Martin Strobel <marstr@microsoft.com>
2018-04-16 15:28:44 -0700

    Adding command skeleton.
    Commands added to:
      - Deploy a dockerized application to Azure. (Presumably this uses AKS)
      - Add an Action to a buffalo application specialized for handling
      Event Grid events.
    
    Fixes #2.

--- cmd/azure.go
+++ cmd/azure.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 Microsoft Corporation and contributors.
+// Copyright © 2018 Microsoft Corporation and contributors
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -18,10 +18,39 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-package main
+package cmd
 
-import "github.com/Azure/buffalo-azure/cmd"
+import (
+	"fmt"
 
-func main() {
-	cmd.Execute()
+	"github.com/spf13/cobra"
+)
+
+// azureCmd represents the azure command
+var azureCmd = &cobra.Command{
+	Use:   "azure",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("azure called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(azureCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// azureCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// azureCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
---
+++ model/eventgrid_generator.go
@@ -0,0 +1,10 @@
+package model
+
+import (
+	"github.com/gobuffalo/makr"
+)
+
+func New() (*makr.Generator, error) {
+	g := makr.New()
+
+}
