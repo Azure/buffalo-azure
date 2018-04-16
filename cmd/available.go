025c740ef3caebf87162f7a2fa1b83f3517f44bb
Martin Strobel <marstr@microsoft.com>
2018-04-16 16:51:36 -0700

    Improving available description.

--- cmd/available.go
+++ cmd/available.go
@@ -31,13 +31,7 @@ import (
 // availableCmd represents the available command
 var availableCmd = &cobra.Command{
 	Use:   "available",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Describes the supported gobuffalo.io commands.",
 	Run: func(cmd *cobra.Command, args []string) {
 		usable := plugins.Commands{
 			{Name: azureCmd.Name(), BuffaloCommand: "root", Description: azureCmd.Short},
