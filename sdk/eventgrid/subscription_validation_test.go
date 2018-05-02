636685126ca78bde35b1e7510b4bcda14a56a829
Martin Strobel <marstr@microsoft.com>
2018-05-02 16:26:16 -0700

    Adding subscriptio validation middleware test coverage.

--- sdk/eventgrid/subscription_validation_test.go
+++ sdk/eventgrid/subscription_validation_test.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"testing"
 
@@ -11,7 +13,7 @@ import (
 	"github.com/gobuffalo/buffalo"
 )
 
-func TestSubscriptionValidationMiddleware(t *testing.T) {
+func TestSubscriptionValidationMiddleware_SubscriptionEvent(t *testing.T) {
 	setFailure := func(c buffalo.Context) error {
 		t.Fail()
 		return c.Error(http.StatusInternalServerError, errors.New("`SubscriptionValidationMiddleware` did not detect request for validation"))
@@ -58,6 +60,66 @@ func TestSubscriptionValidationMiddleware(t *testing.T) {
 
 	if want := "512d38b6-c7b8-40c8-89fe-f46f9e9622b6"; seenResponse.ResponseCode != want {
 		t.Logf("got: %s want: %s", seenResponse.ResponseCode, want)
+		t.Fail()
+	}
+}
+
+func TestSubscriptionValidationMiddleware_OtherEvent(t *testing.T) {
+	var called bool
+	setCalled := func(c buffalo.Context) error {
+		called = true
+		return nil
+	}
+
+	subject := eventgrid.SubscriptionValidationMiddleware(setCalled)
+
+	req, err := http.NewRequest(http.MethodPost, "localhost", bytes.NewReader([]byte(`[{
+		"topic": "/subscriptions/{subscription-id}/resourceGroups/Storage/providers/Microsoft.Storage/storageAccounts/xstoretestaccount",
+		"subject": "/blobServices/default/containers/oc2d2817345i200097container/blobs/oc2d2817345i20002296blob",
+		"eventType": "Microsoft.Storage.BlobCreated",
+		"eventTime": "2017-06-26T18:41:00.9584103Z",
+		"id": "831e1650-001e-001b-66ab-eeb76e069631",
+		"data": {
+			"api": "PutBlockList",
+			"clientRequestId": "6d79dbfb-0e37-4fc4-981f-442c9ca65760",
+			"requestId": "831e1650-001e-001b-66ab-eeb76e000000",
+			"eTag": "0x8D4BCC2E4835CD0",
+			"contentType": "application/octet-stream",
+			"contentLength": 524288,
+			"blobType": "BlockBlob",
+			"url": "https://oc2d2817345i60006.blob.core.windows.net/oc2d2817345i200097container/oc2d2817345i20002296blob",
+			"sequencer": "00000000000004420000000000028963",
+			"storageDiagnostics": {
+			"batchId": "b68529f3-68cd-4744-baa4-3c0498ec19f0"
+			}
+		},
+		"dataVersion": "",
+		"metadataVersion": "1"
+	}]`)))
+
+	req.Header.Add("Content-Type", "application/json")
+
+	ctx := NewMockContext(req)
+
+	err = subject(ctx)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	respBodyLength, err := io.Copy(ioutil.Discard, ctx.Response().(*MockResponseWriter).Body())
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if respBodyLength != 0 {
+		t.Logf("expected an empty body")
+		t.Fail()
+	}
+
+	if called == false {
+		t.Logf("handler pass through never occured")
 		t.Fail()
 	}
 }
