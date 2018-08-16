3bf86a7613c3f9a7c0a71700920033fc585d974b
Martin Strobel <marstr@microsoft.com>
2018-08-16 12:09:17 -0700

    Refactoring to provide queue names with constructor (#80)
    * Refactoring to provide queue names with constructor
    
    * Updating default queue in publish

--- sdk/worker/servicebus_examples_test.go
+++ sdk/worker/servicebus_examples_test.go
@@ -4,19 +4,18 @@ import (
 	"fmt"
 	"os"
 
-	azwork "github.com/Azure/buffalo-azure/worker"
+	azwork "github.com/Azure/buffalo-azure/sdk/worker"
 	"github.com/gobuffalo/buffalo/worker"
 )
 
 func ExampleServiceBusPublisher_initializeAndSend() {
 	sbConnection := "<your service bus connection string>"
-	queueName := "<your queue name>"
 
 	// Create a client that can communicate with a particular Service Bus Queue.
 
 	// Create the client which knows how to publish jobs that will be carried out
 	// by a ServiceBusReceiver which knows how to actually do the job.
-	myPublisher, err := azwork.NewServiceBusPublisher(sbConnection, 1)
+	myPublisher, err := azwork.NewServiceBusPublisher(sbConnection)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		return
@@ -24,7 +23,7 @@ func ExampleServiceBusPublisher_initializeAndSend() {
 
 	// Start sending jobs to be processed somewhere else!
 	myPublisher.Perform(worker.Job{
-		Queue: queueName,
+		Queue: azwork.DefaultQueue,
 		Args: worker.Args{
 			"source": "https://notarealdomain.com/gomodules/buffalo-azure/worker.zip",
 		},
